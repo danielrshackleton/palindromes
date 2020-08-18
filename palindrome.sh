30e7bdc73734968a5968cf2ba9f093de1b0b3368
danielrshackleton <43825001+danielrshackleton@users.noreply.github.com>
2020-08-18 12:50:39 -0700

    Update palindrome.sh

--- palindrome.sh
+++ palindrome.sh
@@ -1,9 +1,6 @@
 #/bin/bash
-####
-####
-####
-####
 
+# Make temporary variables
 
 temp=$(mktemp)
 reverse=$(mktemp)
@@ -12,9 +9,10 @@ reverse1=$(mktemp)
 temp2=$(mktemp)
 reverse2=$(mktemp)
 
+
+# Condense text to list of lowercase, only alphabeticalm unique words, in order of count, and store in 'temp'
 tr '[:upper:]' [:lower:]'  |  tr -cd '[:alpha:]\n '  \
     |  tr ' '  '\n'  |  awk 'length > 2'  |  sort  |  uniq -c  |  sort -nr  >  temp
-
 
 rev temp  >  reverse
 
@@ -27,7 +25,7 @@ rev temp1  >  reverse1
 cat temp1  |  nl  >  temp2
 cat reverse1  |  nl  >  reverse2
 
-
+# Find matches between temp2 and reverse2, and print word count followed by the palindrome itself
 grep -Fxf temp2 reverse2  |  awk '{print $2, $3}'
 
 rm temp1 reverse1 temp2 reverse2
